3195407ede6b722ab6860404e3d3a2acc6bbef4f
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2022-07-20 15:13:29 -0400

    updating index.ts and scripts

--- dist/index.js
+++ dist/index.js
@@ -2,15 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
 const constants_1 = require("./constants");
+const Post_1 = require("./entities/Post");
 const main = async () => {
     const orm = await core_1.MikroORM.init({
-        entities: [__dirname + "/entities/*.ts"],
+        entities: [Post_1.Post],
         dbName: "lireddit",
         user: "postgres",
         password: "postgres",
         type: "postgresql",
         debug: !constants_1.__prod__,
     });
+    const post = orm.em.create(Post_1.Post, { title: "my first post" });
+    await orm.em.persistAndFlush(post);
 };
 main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
 
 const main = async () => {
   const orm = await MikroORM.init({
-    entities: [__dirname + "/entities/*.ts"],
+    entities: [Post],
     dbName: "lireddit",
     user: "postgres",
     password: "postgres",
     type: "postgresql",
     debug: !__prod__,
   });
+
+  const post = orm.em.create(Post, { title: "my first post" });
+  await orm.em.persistAndFlush(post);
 };
 
 main();
