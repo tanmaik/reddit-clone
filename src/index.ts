139d7b30716d33f89454580216b2800eb68b78a6
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2022-07-20 12:31:19 -0400

    adding orm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello world")
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [__dirname + "/entities/*.ts"],
+    dbName: "lireddit",
+    user: "postgres",
+    password: "postgres",
+    type: "postgresql",
+    debug: !__prod__,
+  });
+};
+
+main();
